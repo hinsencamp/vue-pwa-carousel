26d9fab2e8b8b76bdd25d9480d3560b7e8a30eca
Fabian Hinsenkamp <Fabian.Hinsenkamp@camunda.com>
2019-02-09 16:14:52 +0100

    first commit

---
+++ src/components/Card.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="card">
+    <img
+      class="card-icon"
+      :src="require(`@/assets/svg/${imgName}`)"
+      alt="card icon"
+    />
+    <div class='card-content'>
+      <h3 class='headline'>{{headline}}</h3>
+      <p>{{text}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: {
+    headline: String,
+    imgName: String,
+    text: String
+  }
+};
+</script>
+
+<style src="../assets/styles/Card.css" scoped>
---
+++ src/components/Imprint.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class='imprint'>Icons made by <a
+      href="https://www.freepik.com/"
+      title="Freepik"
+    >Freepik</a> from <a
+      href="https://www.flaticon.com/"
+      title="Flaticon"
+    >www.flaticon.com</a> is licensed by <a
+      href="http://creativecommons.org/licenses/by/3.0/"
+      title="Creative Commons BY 3.0"
+      target="_blank"
+    >CC 3.0 BY</a> and
+    <a href="https://thenounproject.com/search/?q=chevron&i=1025828">Landan Lloyd </a> from the Noun Project
+  </div>
+</template>
+
+<style src="../assets/styles/Imprint.css" scoped />
+
+
+
+
+
