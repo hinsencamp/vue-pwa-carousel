de754daf31643f92244169935b00f26242c70beb
Fabian Hinsenkamp <Fabian.Hinsenkamp@camunda.com>
2019-02-10 19:46:55 +0100

    add ArrowButtons

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,22 +1,33 @@
 <template>
   <div>
     <div class="card-carousel">
+      <ArrowButton
+        arrowType="left"
+        @click.native="showPrevElement"
+        :disabled="this.reachedMaxLeft"
+      />
       <Card
         class="current-element"
         :headline="currentElement.headline"
         :text="currentElement.text"
         :imgName="currentElement.imgName"
       />
+      <ArrowButton
+        arrowType="right"
+        @click.native="showNextElement"
+        :disabled="this.reachedMaxRight"
+      />
     </div>
   </div>
 </template>
 <script>
 import Card from "./Card.vue";
+import ArrowButton from "./ArrowButton";
 
 export default {
   name: "Carousel",
   props: { cards: Array },
-  components: { Card },
+  components: { Card, ArrowButton },
 
   data() {
     return {
@@ -26,6 +37,20 @@ export default {
   computed: {
     currentElement() {
       return this.cards[this.currentElementIndex];
+    },
+    reachedMaxLeft() {
+      return this.currentElementIndex === 0;
+    },
+    reachedMaxRight() {
+      return this.currentElementIndex === this.cards.length - 1;
+    }
+  },
+  methods: {
+    showNextElement() {
+      this.currentElementIndex++;
+    },
+    showPrevElement() {
+      this.currentElementIndex--;
     }
   }
 };
