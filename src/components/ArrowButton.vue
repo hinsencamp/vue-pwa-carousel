643fcbc0f1152caca0a6a420c12e7af756c5a536
Fabian Hinsenkamp <Fabian.Hinsenkamp@camunda.com>
2019-02-10 19:46:55 +0100

    add ArrowButtons

---
+++ src/components/ArrowButton.vue
@@ -0,0 +1,39 @@
+<template>
+  <button
+    class="btn"
+    :class="this.styles"
+  >
+    <img
+      class="icon"
+      :src="require(`@/assets/svg/${arrowType}.svg`)"
+      :alt="arrowType + 'arrow'"
+    >
+  </button>
+</template>
+
+<script>
+export default {
+  name: "ArrowButton",
+  props: { arrowType: String },
+
+  data() {
+    return {
+      props: this.$props
+    };
+  },
+  computed: {
+    styles() {
+      const styles = {
+        left: "prev-btn",
+        right: "next-btn"
+      };
+      return styles[this.arrowType];
+    }
+  }
+};
+</script>
+
+<style src="../assets/styles/ArrowButton.css" scoped/>
+
+
+
