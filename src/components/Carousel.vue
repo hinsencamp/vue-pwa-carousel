a798fcdf74692307e4c62e5b64a302ac10ef42f4
Fabian Hinsenkamp <Fabian.Hinsenkamp@camunda.com>
2019-02-10 19:44:24 +0100

    add Carousel

---
+++ src/components/Carousel.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div class="card-carousel">
+      <Card
+        class="current-element"
+        :headline="currentElement.headline"
+        :text="currentElement.text"
+        :imgName="currentElement.imgName"
+      />
+    </div>
+  </div>
+</template>
+<script>
+import Card from "./Card.vue";
+
+export default {
+  name: "Carousel",
+  props: { cards: Array },
+  components: { Card },
+
+  data() {
+    return {
+      currentElementIndex: 0
+    };
+  },
+  computed: {
+    currentElement() {
+      return this.cards[this.currentElementIndex];
+    }
+  }
+};
+</script>
+
+<style src="../assets/styles/Carousel.css" scoped/>
