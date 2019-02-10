76a4dfb13f31d4be62d64867e709cebfbea78807
Fabian Hinsenkamp <Fabian.Hinsenkamp@camunda.com>
2019-02-10 23:50:20 +0100

    add Indicators

---
+++ src/components/Indicators.vue
@@ -0,0 +1,24 @@
+<template>
+  <ul>
+    <li
+      v-for="(_, index) in elements"
+      class="dot"
+      :key="index"
+      :class="{'dot-selected': currentElementIndex === index}"
+    >
+      <button
+        class="indicator-btn"
+        @click='showElement(index)'
+        :disabled=" currentElementIndex === index"
+      />
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "Indicators",
+  props: { elements: Array, currentElementIndex: Number, showElement: Function }
+};
+</script>
+<style src="../assets/styles/Indicators.css" scoped/>
